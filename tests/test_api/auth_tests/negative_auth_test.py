3de972d46890ca72849d98951f347d1eefe9c00e
lohoiigrok <130477515+lohoiigrok@users.noreply.github.com>
2026-04-15 02:05:45 +0700

    refactor negative_auth_test
    Убрал пробелы между значениями аргументов функции и самим аргументами.

--- tests/test_api/auth_tests/negative_auth_test.py
+++ tests/test_api/auth_tests/negative_auth_test.py
@@ -3,7 +3,7 @@ class TestNegativeAuthAPI:
 
     def test_invalid_pass_auth(self, api_manager, test_user):
         invalid_login_data = {"email": test_user["email"], "password": "Dsdjods32d"}
-        response = api_manager.auth_api.login_user(invalid_login_data, expected_status = 401)
+        response = api_manager.auth_api.login_user(invalid_login_data, expected_status=401)
         response_data = response.json()
         # Проверки
         assert "message" in response_data
@@ -13,7 +13,7 @@ class TestNegativeAuthAPI:
 
     def test_invalid_email_auth(self, api_manager, test_user):
         invalid_login_data = {"email": "test_@mail.ru", "password": test_user["password"]}
-        response = api_manager.auth_api.login_user(invalid_login_data, expected_status = 401)
+        response = api_manager.auth_api.login_user(invalid_login_data, expected_status=401)
         response_data = response.json()
         # Проверки
         assert "accessToken" not in response_data, "Токен доступа выдало при неправильных данных"
@@ -22,7 +22,7 @@ class TestNegativeAuthAPI:
 
     def test_empty_body_auth(self, api_manager):
         invalid_login_data = {}
-        response = api_manager.auth_api.login_user(invalid_login_data, expected_status = 401)
+        response = api_manager.auth_api.login_user(invalid_login_data, expected_status=401)
         response_data = response.json()
         # Проверки
         assert "accessToken" not in response_data, "Токен доступа выдало при неправильных данных"
