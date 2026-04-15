4a6225f1f1933520b182e6f3e93a1d2b9b3ed2fe
lohoiigrok <130477515+lohoiigrok@users.noreply.github.com>
2026-04-16 02:12:22 +0700

    Delete comments in constants

--- constants.py
+++ constants.py
@@ -7,10 +7,8 @@ HEADERS = {
 LOGIN_ENDPOINT = "/login"
 REGISTER_ENDPOINT = "/register"
 
-# Дополняем константы
 MOVIE_BASE_URL = "https://api.dev-cinescope.coconutqa.ru"
 MOVIES_ENDPOINT = "/movies"
 
-# Креды SUPER_ADMIN
 SUPER_ADMIN_EMAIL = "api1@gmail.com"
 SUPER_ADMIN_PASSWORD = "asdqwe123Q"
